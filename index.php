10e32f26547989020e2b9f6d9b746334bc782a86
alleswebdev <32772919+alleswebdev@users.noreply.github.com>
2019-01-11 21:58:40 +0300

    Create tool (#2)
    * end memcached
    
    * rename variable
    
    * del brackets

--- index.php
+++ index.php
@@ -3,10 +3,15 @@ require_once("memcached.php");
 
 use \Alleswebdev\Tools\Memcached;
 
-//ob_implicit_flush();
+ob_implicit_flush();
 
 $app = new Memcached();
 $app->connect();
-echo $app->getVersion() . "\r\n";
+$app->set("testkey", "testdata 222 111 32412") . PHP_EOL;
+echo $app->get("testkey")['value'] . PHP_EOL;
+$app->delete("testkey") . PHP_EOL;
+echo $app->stat();
+echo PHP_EOL;
 $app->close();
+
 echo $app::sendCommandEx("version");
