8f065bb8986dbba6314ad27b5c1dca88ae44319e
Alles <aa.alles@bizprofi.ru>
2019-01-13 17:50:06 +0300

    dell debug

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $app = new Memcached();
 $app->connect();
 $app->set("testkey", "testdata 222 111 32412");
 echo $app->get("testkey")['value'] . PHP_EOL;
-$app->delete("testkey") . PHP_EOL;
+$app->delete("testkey");
 echo $app->stat();
 echo PHP_EOL;
 $app->close();
